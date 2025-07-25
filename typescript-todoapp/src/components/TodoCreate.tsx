29b7154bbdf4045e75eceeb03141f2da96b07452
GizemYerlikayaa <giizemyerlikaya@gmail.com>
2025-07-25 18:44:44 +0300

    todo olusturma kısmını kodladım

--- typescript-todoapp/src/components/TodoCreate.tsx
+++ typescript-todoapp/src/components/TodoCreate.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createTodo } from "../redux/todoSlice";
+import type { TodoType } from "../types/Types";
 
 function TodoCreate() {
+  const dispatch = useDispatch();
+  const [newTodo, setNewTodo] = useState<string>("");
+  const handleCreateTodo = () => {
+    if (newTodo.trim().length == 0) {
+      alert("Todo Giriniz !");
+      return;
+    }
+    const payload: TodoType = {
+      id: Math.floor(Math.random() * 9999999999999),
+      content: newTodo,
+    };
+    dispatch(createTodo(payload));
+    setNewTodo("");
+  };
   return (
     <div className="todo-create">
-      <input className="todo-input" placeholder="Todo Giriniz.." type="text" />
-      <button className="todo-create-button">Oluştur</button>
+      <input
+        value={newTodo}
+        onChange={(e: React.ChangeEvent<HTMLInputElem>) =>
+          setNewTodo(e.target.value)
+        }
+        className="todo-input"
+        placeholder="Todo Giriniz.."
+        type="text"
+      />
+      <button onClick={handleCreateTodo} className="todo-create-button">
+        Oluştur
+      </button>
     </div>
   );
 }
